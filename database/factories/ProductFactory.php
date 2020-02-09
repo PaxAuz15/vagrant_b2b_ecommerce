4ce7180ef0e331b76d111522644ad4dd251c4e84
paxauz15 <luisauz.geek@gmail.com>
2020-02-09 10:38:59 -0500

    feat: create 50 fake product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->sentence(2),
+        'description' => $faker->sentence(20),
+        'price' => $faker->numberBetween(100, 5000),
+
+
     ];
 });
