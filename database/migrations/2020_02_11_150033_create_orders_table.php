3dbd58f49173f72c072c973842d59d25462261f7
paxauz15 <luisauz.geek@gmail.com>
2020-02-11 10:19:27 -0500

    edit: create new pharameters on order table

--- database/migrations/2020_02_11_150033_create_orders_table.php
+++ database/migrations/2020_02_11_150033_create_orders_table.php
@@ -15,6 +15,32 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->string('order_number');
+            $table->unsignedBigInteger('user_id');
+            $table->enum('status', ['pending', 'processing', 'completed', 'decline']);
+            $table->float('grand_total');
+            $table->integer('item_count');
+            $table->boolean('is_paid')->default(false);
+            $table->enum('payment_method', ['cash_on _delivery', 'paypal', 'stripe', 'card'])->default('cash_on_delivery');
+
+            $table->string('shipping_fullname');
+            $table->string('shipping_address');
+            $table->string('shipping_city');
+            $table->string('shipping_state');
+            $table->string('shipping_zipcode');
+            $table->string('shipping_phone');
+            $table->string('notes')->nullable();
+
+            $table->string('billing_fullname');
+            $table->string('billing_address');
+            $table->string('billing_city');
+            $table->string('billing_state');
+            $table->string('billing_zipcode');
+            $table->string('billing_phone');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
