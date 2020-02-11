6cedd2a8d12dd474ba7cf73bc81f65883bb42453
paxauz15 <luisauz.geek@gmail.com>
2020-02-11 10:34:31 -0500

    feat: form to shipping order

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -6,5 +6,43 @@
 
     <h3>Shipping Information</h3>
 
+    <form action="{{ route('orders.store') }}">
+
+        @csrf
+
+        @include('_errors')
+
+        <div class="form-group">
+          <label for="">Full Name</label>
+          <input type="text" name="shipping_fullname" id="" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="">State</label>
+            <input type="text" name="shipping_state" id="" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="">City</label>
+            <input type="text" name="shipping_city" id="" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="">Zip</label>
+            <input type="text" name="shipping_zipcode" id="" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="">Full Address</label>
+            <input type="text" name="shipping_address" id="" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="">Mobile</label>
+            <input type="text" name="shipping_phone" id="" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Place Order</button>
+    </form>
 
 @endsection
