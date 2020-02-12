ef41d4aa664656918aa0b940ce19216ba04259e1
paxauz15 <luisauz.geek@gmail.com>
2020-02-12 08:48:06 -0500

    add: new feat on PayPalController

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use Darryldecode\Cart\Cart;
 use Illuminate\Http\Request;
+use Srmklive\PayPal\Services\ExpressCheckout;
 
 class PayPalController extends Controller
 {
-    //
+    public function getExpressCheckout()
+    {
+        $cart = Cart::session(auth()->id());
+        // $cartItems = [
+        //     [
+        //         'name'  => 'Product 1',
+        //         'price' => 9.99,
+        //         'qty'   => 1,
+        //     ],
+        //     [
+        //         'name'  => 'Product 2',
+        //         'price' => 4.99,
+        //         'qty'   => 2,
+        //     ],
+        // ];
+
+        $cartItems = array_map($cart->getContent());
+
+
+        $checkoutData = [
+            'items'=>$cartItems,
+
+            'return_url'=>route('paypal.success'),
+            'cancel_url'=>route('paypal.cancel'),
+            'invoice_id'=>uniqid(),
+            'invoice_description'=> " Order description ",
+            'total'=>$cart->getTotal()
+        ];
+
+        $provider = new ExpressCheckout();
+
+        $provider->setExpressCheckout($checkoutData);
+    }
+
+    public function cancelPage()
+    {
+        dd('payment failed');
+    }
 }
