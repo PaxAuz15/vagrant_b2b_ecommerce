24284b9164d053e9778ee84f5b6643f8aab5d53d
paxauz15 <luisauz.geek@gmail.com>
2020-02-12 11:49:10 -0500

    fix: dd($response) on PayPalController works! Just for credentials users paypal on .env

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -35,7 +35,8 @@ class PayPalController extends Controller
 
         $response = $provider->setExpressCheckout($checkoutData);
 
-        dd($response);
+        dd($response); //works!
+
     }
 
     public function cancelPage()
