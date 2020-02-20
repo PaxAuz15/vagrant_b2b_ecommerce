115595932afe8df55dc7091943e9292f5c40085b
paxauz15 <luisauz.geek@gmail.com>
2020-02-20 09:50:56 -0500

    feat: only show shops created for user logged.

--- app/Http/Controllers/Admin/ShopController.php
+++ app/Http/Controllers/Admin/ShopController.php
@@ -66,6 +66,10 @@ class ShopController extends VoyagerBaseController
                 $query = $model::select('*');
             }
 
+            //query to display seller's shop only
+
+            $query->where('user_id', auth()->id());
+
             // Use withTrashed() if model uses SoftDeletes and if toggle is selected
             if ($model && in_array(SoftDeletes::class, class_uses_recursive($model)) && Auth::user()->can('delete', app($dataType->model_name))) {
                 $usesSoftDeletes = true;
