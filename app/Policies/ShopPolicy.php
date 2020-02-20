10ebc93c4ab752caf102d113571919475aeef751
paxauz15 <luisauz.geek@gmail.com>
2020-02-20 11:09:32 -0500

    feat: agg new policies on seller role with shop admin panel.

--- app/Policies/ShopPolicy.php
+++ app/Policies/ShopPolicy.php
@@ -15,28 +15,22 @@ class ShopPolicy
         return $user->hasRole('seller');
     }
 
-    /**
-     * Determine whether the user can view any shops.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the shop.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Shop  $shop
-     * @return mixed
-     */
-    public function view(User $user, Shop $shop)
+    public function read(User $user, Shop $shop)
     {
         return $user->id == $shop->user_id;
     }
+
+    /**
+     * Determine whether the user can update any shops.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+
+     public function edit(User $user, Shop $shop)
+     {
+         return $user->id == $shop->user_id;
+     }
 
     /**
      * Determine whether the user can create shops.
@@ -44,21 +38,9 @@ class ShopPolicy
      * @param  \App\User  $user
      * @return mixed
      */
-    public function create(User $user)
+    public function add(User $user)
     {
         //
-    }
-
-    /**
-     * Determine whether the user can update the shop.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Shop  $shop
-     * @return mixed
-     */
-    public function update(User $user, Shop $shop)
-    {
-        return $user->id == $shop->user_id;
     }
 
     /**
@@ -70,30 +52,6 @@ class ShopPolicy
      */
     public function delete(User $user, Shop $shop)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the shop.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Shop  $shop
-     * @return mixed
-     */
-    public function restore(User $user, Shop $shop)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the shop.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Shop  $shop
-     * @return mixed
-     */
-    public function forceDelete(User $user, Shop $shop)
-    {
-        //
+        return $user->id == $shop->user_id;
     }
 }
