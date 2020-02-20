318db55e9a44be7d55e1a42978ebae8c48eb9d58
paxauz15 <luisauz.geek@gmail.com>
2020-02-20 10:40:05 -0500

    add: php artisan make:policy ShopPolicy --model=Shop

---
+++ app/Policies/ShopPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Shop;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ShopPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any shops.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the shop.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Shop  $shop
+     * @return mixed
+     */
+    public function view(User $user, Shop $shop)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create shops.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the shop.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Shop  $shop
+     * @return mixed
+     */
+    public function update(User $user, Shop $shop)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the shop.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Shop  $shop
+     * @return mixed
+     */
+    public function delete(User $user, Shop $shop)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the shop.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Shop  $shop
+     * @return mixed
+     */
+    public function restore(User $user, Shop $shop)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the shop.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Shop  $shop
+     * @return mixed
+     */
+    public function forceDelete(User $user, Shop $shop)
+    {
+        //
+    }
+}
