ed2788ba3f397bc00f34d2af7c2cce93bd73c5e0
paxauz15 <luisauz.geek@gmail.com>
2020-03-12 10:10:59 -0500

    add&fix: MenusTableSeeder
    source: commit 39fcc77e19

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -12,11 +12,19 @@ class MenusTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('menus')->delete();
-        
-        
-        
+
+        \DB::table('menus')->insert(array (
+            0 =>
+            array (
+                'id' => 1,
+                'name' => 'admin',
+                'created_at' => '2020-02-14 09:49:08',
+                'updated_at' => '2020-02-14 09:49:08',
+            ),
+        ));
+
     }
 }
