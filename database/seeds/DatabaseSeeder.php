e7d60c1c8d69cf38258745b70b73ba3f4001e3e9
paxauz15 <luisauz.geek@gmail.com>
2020-03-12 11:41:12 -0500

    add: Table Seeder
    source: https://github.com/webdevmatics/webmall/commit/99cbc3fbc15dc8fdc11b84acbeac07ca134ce59c

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->call(ProductSeeder::class);
         $this->call(DataTypesTableSeeder::class);
         $this->call(DataRowsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
@@ -21,12 +20,16 @@ class DatabaseSeeder extends Seeder
         $this->call(MenusTableSeeder::class);
         $this->call(MenuItemsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
-        $this->call(UserRolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(PermissionRoleTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
 
         $this->call(UsersTableSeeder::class);
+
+        $this->call(ShopSeeder::class);
+        $this->call(ProductSeeder::class);
+
+        $this->call(UserRolesTableSeeder::class);
 
     }
 }
