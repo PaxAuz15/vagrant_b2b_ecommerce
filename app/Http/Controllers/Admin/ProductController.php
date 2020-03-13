dab0270cdf7baebdc04afeb6ce2bb9e8fb631d1d
paxauz15 <luisauz.geek@gmail.com>
2020-03-13 10:20:49 -0500

    add&feat: agg dependencies

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use TCG\Voyager\Facades\Voyager;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Events\BreadDataDeleted;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use TCG\Voyager\Database\Schema\SchemaManager;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
 class ProductController extends Controller
 {
