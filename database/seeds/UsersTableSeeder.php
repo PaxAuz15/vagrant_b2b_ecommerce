7967b68739ac29b25371dbd204d49be2fa438b43
paxauz15 <luisauz.geek@gmail.com>
2020-03-13 09:24:01 -0500

    refactoring: UsersTableSeeder
    source: webdevmatics/webmall@99cbc3f

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -73,6 +73,34 @@ class UsersTableSeeder extends Seeder
                 'created_at' => '2020-02-09 14:02:38',
                 'updated_at' => '2020-02-14 09:55:11',
             ),
+            4 =>
+            array (
+                'id' => 5,
+                'role_id' => 3,
+                'name' => 'Seller 1',
+                'email' => 'seller1@webmall.com',
+                'avatar' => 'users/default.png',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$NJBQDBmcH.xG9nSx9rT4j.EY4z1dpqGyyatOCwaz/8r0fMU.pST1i',
+                'remember_token' => NULL,
+                'settings' => NULL,
+                'created_at' => '2020-02-20 00:16:34',
+                'updated_at' => '2020-02-20 00:16:34',
+            ),
+            5 =>
+            array (
+                'id' => 6,
+                'role_id' => 3,
+                'name' => 'Seller 2',
+                'email' => 'seller2@webmall.com',
+                'avatar' => 'users/default.png',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$NJBQDBmcH.xG9nSx9rT4j.EY4z1dpqGyyatOCwaz/8r0fMU.pST1i',
+                'remember_token' => NULL,
+                'settings' => NULL,
+                'created_at' => '2020-02-20 00:16:34',
+                'updated_at' => '2020-02-20 00:16:34',
+            ),
         ));
 
 
